141ea8979b7bf6ccaaf232b5e09f32739e326778
Álvaro Nicolás Pérez Mella <alvaronico.p@gmail.com>
2026-05-25 19:01:02 +0200

    chore(docker): añadir opción de despligue mediante docker

--- src/run.py
+++ src/run.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     Punto de entrada principal del servidor.
     - host '0.0.0.0': Permite la escucha en todas las interfaces de red locales.
     - port 5000: Puerto estándar de comunicación para el servicio.
-    - debug: Habilitado para facilitar el desarrollo y hot-reloading.
+    - debug: Deshabilitado para producción.
     """
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
